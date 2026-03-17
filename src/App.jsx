e4357d26b39e332cf49ac66cb7f25d104cfaee61
Minji Kim <minji.kim@tui.se>
2026-03-17 13:52:22 +0100

    test

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <Header />
         <main>
           <Routes>
+            {/* test */}
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/about" element={<About />} />
